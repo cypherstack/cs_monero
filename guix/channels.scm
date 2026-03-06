1d315e5fdf8575b8b183f90fa8195d2e39f8e2d6
sneurlax <sneurlax@gmail.com>
2026-03-06 13:48:03 -0600

    feat(guix): add channel pinning and flutter version config

---
+++ guix/channels.scm
@@ -0,0 +1,9 @@
+;; Pinned Guix channel commits for reproducible builds.
+;; Regenerate with: guix/pin-channels.sh
+;; Or: make -C guix pin
+;;
+;; This file is auto-generated. Do not edit manually.
+(list (channel
+        (name 'guix)
+        (url "https://git.guix.gnu.org/guix.git")
+        (branch "master")))
